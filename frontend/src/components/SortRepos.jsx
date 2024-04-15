739c219759e94c010d2bf70f5ca65229099efed2
houssemchatibi <houssem.chatibi@gmail.com>
2024-04-15 02:02:08 +0100

    sortType

--- frontend/src/components/SortRepos.jsx
+++ frontend/src/components/SortRepos.jsx
@@ -1,31 +1,27 @@
 import React from 'react'
 
-const SortRepos = () => {
-  return (
-    <div className='mb-2 flex justify-center space-x-4 lg:justify-end'>
-        
-        
-      <button
-      type='button'
-       className='py-2.5 px-5 mb-2 text-xs sm:text- font-medium focus:outline-none rounded-lg bg-glass'>
-      Most Reccent
-      </button>
-      <button
-      type='button'
-       className='py-2.5 px-5 mb-2 text-xs sm:text- font-medium focus:outline-none rounded-lg bg-glass'>
-      Most Reccent
-      </button>
-      <button
-      type='button'
-       className='py-2.5 px-5 mb-2 text-xs sm:text- font-medium focus:outline-none rounded-lg bg-glass'>
-      Most Reccent
-      </button>
-    </div>
-  )
-}
+const SortRepos = ({ onSort, sortType }) => {
+	const BUTTONS = [
+		{ type: "recent", text: "Most Recent" },
+		{ type: "stars", text: "Most Stars" },
+		{ type: "forks", text: "Most Forks" },
+	];
 
-export default SortRepos
-
-// <div className='py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass'>
-      
-    // </div>
+	return (
+		<div className='mb-2 flex justify-center lg:justify-end'>
+			{BUTTONS.map((button) => (
+				<button
+					key={button.type}
+					type='button'
+					className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${
+						button.type == sortType ? "border-blue-500" : ""
+					}`}
+					onClick={() => onSort(button.type)}
+				>
+					{button.text}
+				</button>
+			))}
+		</div>
+	);
+};
+export default SortRepos;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -54,6 +54,17 @@ const HomePage = () => {
 		setLoading(false);
 		console.log("userrrr",userProfile)
 	}
+	const onSort = (sortType) => {
+		if (sortType === "recent") {
+			repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
+		} else if (sortType === "stars") {
+			repos.sort((a, b) => b.stargazers_count - a.stargazers_count); //descending, most stars first
+		} else if (sortType === "forks") {
+			repos.sort((a, b) => b.forks_count - a.forks_count); //descending, most forks first
+		}
+		setSortType(sortType);
+		setRepos([...repos]);
+	};
 
 
 	useEffect(() => {
@@ -62,7 +73,7 @@ const HomePage = () => {
 	return (
 		<div className='m-4'>
 			<Search onSearch={onSearch}/>
-			<SortRepos />
+			{repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
 			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
 				{userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
 				{ !loading && <Repos repos={repos }  />}
